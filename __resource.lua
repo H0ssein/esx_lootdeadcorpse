c924df737c015e72a36f8d41f379aeac5990f054
H0ssein <hossein.813@gmail.com>
2019-10-28 05:20:18 +0330

    added swedish locale
    thanks to https://forum.fivem.net/u/Baiter_YT

--- __resource.lua
+++ __resource.lua
@@ -3,6 +3,7 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 client_scripts {
     '@es_extended/locale.lua',
     'locales/en.lua',
+	'locales/sv.lua',
     'config.lua',
     'client/main.lua'
 }
@@ -10,6 +11,7 @@ client_scripts {
 server_scripts {
     '@es_extended/locale.lua',
     'locales/en.lua',
+	'locales/sv.lua',
     'config.lua',
 	'server/main.lua'
 }
---
+++ locales/sv.lua
@@ -0,0 +1,20 @@
+Locales['sv'] = {
+    ['cash']                = 'Pengar',--
+    ['black_money']         = 'svarta pengar',--
+    ['inventory']           = 'Lager',--
+	['gun_label']           = 'Vapen',
+    ['target_inventory']    = 'Dödas Lager',
+    ['steal']               = 'Sno',
+    ['return']              = 'Ge tillbacka',
+    ['action_choice']       = 'Välj åtgärd',
+    ['amount']              = 'Summa',
+    ['ex_inv_lim_source']   = 'Brist på plats i lager',
+    ['ex_inv_lim_target']   = 'Brist på plats i lager',
+    ['you_stole']           = 'Du stal Från den här personen',
+    ['from_your_target']    = 'Från denna Personen',
+    ['someone_stole']       = 'Någon stal från dig',
+    ['invalid_quantity']    = 'Fel Summa',
+}
+
+
+-- Thanks to https://forum.fivem.net/u/Baiter_YT
